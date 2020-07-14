5a62d1816868cb13d90787221e7f48b84c671a7b
unknown <amienkowski@gmail.com>
2020-07-14 14:50:49 +0200

    Test metody wybierającej parzyste liczby z kolekcji

---
+++ src/test/java/com/kodilla/KodillaCourseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.kodilla.testing.user.SimpleUser
+
+@SpringBootTest
+class KodillaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
